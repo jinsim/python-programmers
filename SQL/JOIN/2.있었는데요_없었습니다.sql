bbd8dc73c72866bebb018a005ed1e99d07babbd7
jinsim <simsime@korea.ac.kr>
2022-09-04 09:57:47 +0900

    있었는데요 없었습니다

---
+++ SQL/JOIN/2.있었는데요_없었습니다.sql
@@ -0,0 +1,10 @@
+/*
+(INNER) JOIN 을 하면 교집합에 해당하는 컬럼을 얻을 수 있다. ID를 ON으로 잡고 JOIN한다.
+입양일보다 들어온 날짜가 더 커야하므로, WHERE 절을 설정해주고, 들어온 날짜를 기준으로 정렬해준다.
+*/
+SELECT INS.ANIMAL_ID, INS.NAME
+FROM ANIMAL_INS AS INS
+JOIN ANIMAL_OUTS AS OUTS
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE INS.DATETIME > OUTS.DATETIME
+ORDER BY INS.DATETIME
